a156b7fd3fdc19c84d12d4d94555af966825a380
sparnet <alessandropanait69@gmail.com>
2024-04-15 18:37:20 +0200

    Iniziato lo score. Player data anche esso iniziato, il quale bukkit salverà i dati in YAML.

--- src/main/java/queryys/hubcombat/Data/PlayerData.java
+++ src/main/java/queryys/hubcombat/Data/PlayerData.java
@@ -1,4 +1,56 @@
 package queryys.hubcombat.Data;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import queryys.hubcombat.HubCombat;
+
+import java.io.File;
+import java.io.IOException;
+
 public class PlayerData {
+
+    private final HubCombat plugin;
+    private final File playerDataFile;
+    private final FileConfiguration playerDataConfig;
+
+    public PlayerData(HubCombat plugin) {
+        this.plugin = plugin;
+        this.playerDataFile = new File(plugin.getDataFolder(), "player_data.yml");
+        this.playerDataConfig = YamlConfiguration.loadConfiguration(playerDataFile);
+    }
+
+    public void savePlayerData(Player player) {
+        String playerName = player.getName();
+        // Esempio di salvataggio di dati per il giocatore
+        playerDataConfig.set(playerName + ".kills", getPlayerKills(player));
+        playerDataConfig.set(playerName + ".deaths", getPlayerDeaths(player));
+
+        // Salva i dati su file
+        try {
+            playerDataConfig.save(playerDataFile);
+        } catch (IOException e) {
+            plugin.getLogger().warning("Impossibile salvare i dati del giocatore " + playerName);
+        }
+    }
+
+    public int getPlayerKills(Player player) {
+        // Restituisci il numero di uccisioni del giocatore
+        return playerDataConfig.getInt(player.getName() + ".kills", 0);
+    }
+
+    public void setPlayerKills(Player player, int kills) {
+        // Imposta il numero di uccisioni del giocatore
+        playerDataConfig.set(player.getName() + ".kills", kills);
+    }
+
+    public int getPlayerDeaths(Player player) {
+        // Restituisci il numero di morti del giocatore
+        return playerDataConfig.getInt(player.getName() + ".deaths", 0);
+    }
+
+    public void setPlayerDeaths(Player player, int deaths) {
+        // Imposta il numero di morti del giocatore
+        playerDataConfig.set(player.getName() + ".deaths", deaths);
+    }
 }
--- src/main/java/queryys/hubcombat/score/CombatScoreManager.java
+++ src/main/java/queryys/hubcombat/score/CombatScoreManager.java
@@ -1,4 +1,38 @@
 package queryys.hubcombat.score;
 
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class CombatScoreManager {
+
+    private final Map<Player, Integer> kills = new HashMap<>();
+    private final Map<Player, Integer> deaths = new HashMap<>();
+
+    public void addKill(Player player) {
+        int currentKills = kills.getOrDefault(player, 0);
+        kills.put(player, currentKills + 1);
+    }
+
+    public void addDeath(Player player) {
+        int currentDeaths = deaths.getOrDefault(player, 0);
+        deaths.put(player, currentDeaths + 1);
+    }
+
+    public int getKills(Player player) {
+        return kills.getOrDefault(player, 0);
+    }
+
+    public int getDeaths(Player player) {
+        return deaths.getOrDefault(player, 0);
+    }
+
+    public int getScore(Player player) {
+        int playerKills = getKills(player);
+        int playerDeaths = getDeaths(player);
+
+        // Calcola lo score in base al numero di uccisioni e morti
+        return playerKills - playerDeaths;
+    }
 }
