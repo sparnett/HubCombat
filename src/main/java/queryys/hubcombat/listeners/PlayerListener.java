c13f26963d303e8aaad1293ed740619625487121
sparnet <alessandropanait69@gmail.com>
2024-04-15 18:09:59 +0200

    Iniziati I CombatManager & PlayerListener. Cosi come l'hubcombat ( java class main ), che presenta ancora alcuni warnings.

--- src/main/java/queryys/hubcombat/listeners/PlayerListener.java
+++ src/main/java/queryys/hubcombat/listeners/PlayerListener.java
@@ -1,4 +1,29 @@
 package queryys.hubcombat.listeners;
 
-public class PlayerListener {
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import queryys.hubcombat.managers.CombatManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PlayerListener implements Listener {
+
+    private final CombatManager combatManager;
+
+    public PlayerListener(JavaPlugin plugin) {
+        this.combatManager = new CombatManager(plugin);
+    }
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        if (event.getItem() != null && event.getItem().getType() == Material.DIAMOND_SWORD) {
+            Player player = event.getPlayer();
+            if (!combatManager.isInCombat(player)) {
+                combatManager.enterCombat(player);
+                combatManager.startCooldown(player);
+            }
+        }
+    }
 }
--- src/main/java/queryys/hubcombat/managers/CombatManager.java
+++ src/main/java/queryys/hubcombat/managers/CombatManager.java
@@ -1,4 +1,38 @@
 package queryys.hubcombat.managers;
 
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.HashMap;
+
 public class CombatManager {
+
+    private final HashMap<Player, Long> combatCooldowns = new HashMap<>();
+    private final JavaPlugin plugin;
+
+    public CombatManager(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    public boolean isInCombat(Player player) {
+        return combatCooldowns.containsKey(player);
+    }
+
+    public void enterCombat(Player player) {
+        combatCooldowns.put(player, System.currentTimeMillis() + 10000L);
+    }
+
+    public void exitCombat(Player player) {
+        combatCooldowns.remove(player);
+    }
+
+    public void startCooldown(Player player) {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                exitCombat(player);
+            }
+        }.runTaskLater(plugin, 200);
+    }
 }
